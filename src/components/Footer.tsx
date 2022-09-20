f2e42c8ca37786dcb3d26669b0ec62366fc31e8f
sanntozzz <jfilipesantos550@gmail.com>
2022-09-20 18:34:44 +0100

    Fix Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,18 +6,18 @@ export function Footer() {
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 1440 320"
+                className="h-10 w-full bg-orange-400"
                 preserveAspectRatio="none"
-                className="h-20 w-full bg-orange-400"
             >
                 <path
-                    className="fill-slate-50"
+                    className="fill-white"
                     d="M0,64L48,80C96,96,192,128,288,128C384,128,480,96,576,96C672,96,768,128,864,149.3C960,171,1056,181,1152,160C1248,139,1344,85,1392,58.7L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
                 ></path>
             </svg>
             <div className="bg-orange-400 text-white">
-                <div className="space-y-2 px-4 pb-6 text-center text-sm">
-                    <p>Developed by João F.C. Santos</p>
-                    <div className="flex items-center justify-center space-x-2 text-lg">
+                <div className="mx-auto max-w-screen-md space-y-2 px-4 pb-6 text-center">
+                    <div className="text-sm">Developed by João F.C. Santos</div>
+                    <div className="flex items-center justify-center gap-2 text-lg">
                         <a
                             href="https://www.linkedin.com/in/sanntozzz/"
                             target="_blank"
@@ -34,7 +34,7 @@ export function Footer() {
                             <Si.SiGithub />
                         </a>
                     </div>
-                    <p className="mx-auto max-w-screen-md text-xs">
+                    <p className="text-xs">
                         This site was created to showcase the developer's skills
                         and experience. The WHAT2EAT brand is a factitious brand
                         and was created just for this demo site.
